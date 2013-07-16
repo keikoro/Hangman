0caab6b9f5e2b84b41b626f0eed6bda57349e360
K Kollmann <git@k.kollmann.moe>
2013-07-16 07:56:43 +0200

    added soundcheck with try/except

--- hangman.py
+++ hangman.py
@@ -11,6 +11,7 @@
 # - retrieve words-to-be-guessed from a file (with minimum length of e.g. 5 letters/word)
 # - build hangman (ASCII graphics)
 # - umlauts are two letters!
+# - delay for say? (so as to display text earlier)
 #
 # Further ideas:
 # - let user guess the entire word
@@ -45,7 +46,27 @@ mywords = ["cherry", "summer", "winter", "programming", "hydrogen", "Saturday",
 			"baking", "sherlock", "troll", "batman", "japan", "pastries", "Cairo",
 			"Vienna", "raindrop", "waves", "diving", "Malta", "cupcake", "ukulele"]
 
-output("Let's play hangman: guess the word!")
+sound = False
+
+print("Starting sound check:")
+# using try to test existence of say subprocess
+try:
+	subprocess.call(["say", "soundcheck, testing, 1, 2, 3"])
+	subprocess.call(["say", "If you hear the voice, please press yes and the enter key."])
+except:
+	print("Please install the programm 'say' to use audio output (or contact your system administrator).")
+	print("Using this program will still work by the way, only without sound.")
+else:
+	print("It seems you have 'say' installed which makes audio output possible.")
+	print("Could you hear your computer say 'soundcheck, testing, 1, 2, 3'?")
+	answer = input("Please type yes or no (and press enter): ")
+	if answer.lower()[0] == 'y':
+		sound = True
+finally:
+	print("Soundcheck completed, you're ready to go!")
+
+
+output("Let's play hangman: guess the word!", voice=sound)
 
 theword = random.choice(mywords).upper()
 
@@ -56,23 +77,24 @@ correctguesses = ''
 tryword = 'tries'
 
 output(stringtogether(placeholder), voice=False) # show blank word
-subprocess.call(["say", "The word you're looking for has {} letters".format(len(theword))])
+if sound:
+	subprocess.call(["say", "The word you're looking for has {} letters".format(len(theword))])
 
 while possibletries > 0:
 
-	output("Please pick a letter: ", ending='')
+	output("Please pick a letter: ", ending='', voice=sound)
 	pickedletter = input().upper()
 
 	if not pickedletter.isalpha() : # don't allow digits, special characters
-		output("Sorry, but that was not a letter! Try again.".format(pickedletter))
+		output("Sorry, but that was not a letter! Try again.".format(pickedletter), voice=sound)
 		continue
 
 	if pickedletter in incorrectguesses: # no multiple guesses of same letter
-		output("You already guessed the letter '{}'!".format(pickedletter))
+		output("You already guessed the letter '{}'!".format(pickedletter), voice=sound)
 		continue
 
 	if pickedletter in correctguesses:
-		output("You already picked {}!".format(pickedletter.upper()))
+		output("You already picked {}!".format(pickedletter.upper()), voice=sound)
 		continue
 
 	if pickedletter in theword.upper(): # guess was correct
@@ -84,36 +106,37 @@ while possibletries > 0:
 				placeholder[letterposition] = theword[letterposition]
 			letterposition += 1
 
-		output("You guessed correctly, well done!")
+		output("You guessed correctly, well done!", voice=sound)
 		correctguesses += pickedletter
 
 	else: # guess was incorrect
 		incorrectguesses += pickedletter			
 		possibletries -= 1
 
-		output("Sorry, '{}' is an incorrect guess!".format(pickedletter.upper()))
+		output("Sorry, '{}' is an incorrect guess!".format(pickedletter.upper()), voice=sound)
 
 		if (incorrectguesses != '') and (possibletries > 0):
 			if possibletries == 1:
 				tryword = 'try' 
 
-			output("You have {} {} left.".format(possibletries,tryword))
-			output("Incorrectly guessed letters so far: ", ending='')
-			output(incorrectguesses, ending='', spell=True)
+			output("You have {} {} left.".format(possibletries,tryword), voice=sound)
+			output("Incorrectly guessed letters so far: ", ending='', voice=sound)
+			output(incorrectguesses, ending='', spell=True, voice=sound)
 			print(".")
 			
 	if not "." in placeholder:
-		output("We have a winner! Thanks for playing. :)")
+		output("We have a winner! Thanks for playing. :)", voice=sound)
 		break
 
 	if placeholder == list("."*len(theword)):
 		output(stringtogether(placeholder), voice=False) # show blank word
-		subprocess.call(["say", "The word you're looking for has {} letters".format(len(theword))])
+		if sound:
+			subprocess.call(["say", "The word you're looking for has {} letters".format(len(theword))])
 	else:
-		output("The word is: ", ending='')
-		output(stringtogether(placeholder),spell=True)
+		output("The word is: ", ending='', voice=sound)
+		output(stringtogether(placeholder),spell=True, voice=sound)
 
 else:
-	output("\nGame over. :( ")
+	output("\nGame over. :( ", voice=sound)
 
-output("The word you were looking for was: " +theword+ ".")
+output("The word you were looking for was: " +theword+ ".", voice=sound)
